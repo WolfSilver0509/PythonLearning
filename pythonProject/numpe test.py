17f345ec683e3e24feab858b17843bf1d1ded4bb
Denizot <denizot.myriam@gmail.com>
2022-04-27 07:38:22 +0200

    Avancement chapitre 1 del'env finis

---
+++ pythonProject/numpe test.py
@@ -0,0 +1,16 @@
+#import numpy
+
+
+#print(numpy.ceil(1.2))
+
+#print(numpy.ceil(1.933))
+
+#print(numpy.floor(1.2))
+
+# le faire en important avec un alias .
+
+import numpy as np
+
+print(np.ceil(1.2))
+print(np.ceil(1.933))
+print(np.floor(1.2))
